0aa783d480b7595d388a317bcae5e710014ce3b7
Steve Mitchell <mitchels.umd@gmail.com>
2019-05-01 16:42:00 -0400

    Create sanitycheck.py

---
+++ sanitycheck.py
@@ -0,0 +1,19 @@
+
+import numpy as np
+import matplotlib
+import cv2
+import imutils
+
+print "All packages imported properly!"
+
+image = cv2.imread("testudo.jpg")
+
+cv2.imshow("Old School Testudo Logo: Original Dimensions", image)
+cv2.waitKey(0)
+
+image = imutils.resize(image, width=400)
+
+cv2.imshow("Old School Testudo Logo: Resized", image)
+cv2.waitKey(0)
+
+cv2.imwrite("testimage.jpg", image)
